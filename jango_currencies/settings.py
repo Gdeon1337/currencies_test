5fc868bc4feeb81ec6f67d7b6bae2cc400451cfd
Gdeon1337 <gdeom1337@gmail.com>
2019-12-19 15:58:38 +0400

    refactor settings.py

--- jango_currencies/settings.py
+++ jango_currencies/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import datetime
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from celery.schedules import crontab
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
